7448afa8e3e8212620a0fdf7993673be816fe86f
kbvstmd <kbvstmd@gmail.com>
2022-04-03 00:01:25 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 wget -c --no-passive-ftp ftp://119.3.70.71/ProGeo-neo2.0/test.tar.gz
+wget -c --no-passive-ftp ftp://118.31.70.55/liyuyu/ProGro-neo/test/rna.tar.gz
 wget -c --no-passive-ftp ftp://119.3.70.71/ProGeo-neo2.0/software.tar.gz
 wget -c --no-passive-ftp ftp://119.3.70.71/ProGeo-neo2.0/reference_file.tar.gz
+wget -c --no-passive-ftp ftp://118.31.70.55/liyuyu/ProGro-neo/reference_files/dbsnp_146.tar.gz
 tar -zxvf *.gz
