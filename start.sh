5944de1356842748d422241d941f4ac86caf1654
kbvstmd <kbvstmd@gmail.com>
2022-04-06 21:40:19 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-wget -c http://118.31.70.55/ProGro-neo/data/dbsnp_146.tar.gz
-wget -c http://118.31.70.55/ProGro-neo/data/rna.tar.gz
+wget -c http://118.31.70.55/ProGeo-neo/data/dbsnp_146.tar.gz
+wget -c http://118.31.70.55/ProGeo-neo/data/rna.tar.gz
 wget -c http://119.3.70.71/ProGeo-neo2.0/data/reference_file.tar.gz
 wget -c http://119.3.70.71/ProGeo-neo2.0/data/software.tar.gz
 wget -c http://119.3.70.71/ProGeo-neo2.0/data/test.tar.gz
